a5ef7bd989976751986d367b7b2f472eac263e4f
Iftesum-Zami <tmizami20@gmail.com>
2020-09-21 04:21:54 +0600

    react auth recape

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7mqh6GngNaMF6HcUAKNfFs2ZeyyGVi7E",
+    authDomain: "burj-al-arab-m44.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-m44.firebaseio.com",
+    projectId: "burj-al-arab-m44",
+    storageBucket: "burj-al-arab-m44.appspot.com",
+    messagingSenderId: "199596427217",
+    appId: "1:199596427217:web:d166ce5746e7228132c2f0"
+  };
+
+  export default firebaseConfig;
